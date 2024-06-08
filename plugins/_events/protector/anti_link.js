447365413c49d14d9c9f8f60f2e2282ac6656276
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-06-08 18:11:02 +0700

    Update anti_link.js

--- plugins/_events/protector/anti_link.js
+++ plugins/_events/protector/anti_link.js
@@ -8,7 +8,7 @@ exports.run = {
    }) => {
       try {
          // delete link then kick when antilink is turned on
-         if (groupSet.antilink && m.isGroup && body) {
+         if (groupSet.antilink && m.isGroup && body && !m.fromMe) {
             if (body.match(/(https?:\/\/)?(www\.)?(chat\.whatsapp\.com)(\/[^\s]*)?/gi) && !body.includes(await client.groupInviteCode(m.chat)) || body.match(/(wa.me)/gi)) return client.sendMessage(m.chat, {
                delete: {
                   remoteJid: m.chat,
