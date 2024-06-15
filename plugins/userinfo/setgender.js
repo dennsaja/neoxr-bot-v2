b035fefd4cf946fc4686fa806456d932a83abb37
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-06-15 14:55:26 +0700

    add setgender

---
+++ plugins/userinfo/setgender.js
@@ -0,0 +1,24 @@
+exports.run = {
+    usage: ['setgender'],
+    use: '(option)',
+    category: 'user info',
+    async: async (m, {
+       client,
+       args,
+       isPrefix,
+       command,
+       Func
+    }) => {
+       try {
+        let user = global.db.users.find(v => v.jid == m.sender)
+          if (!args || !args[0]) return m.reply(Func.example(isPrefix, command, 'cowok'))
+          if (!['cowok','cewek'].includes(args[0])) return client.reply(m.chat, Func.texted('bold', `🚩 Gender tidak ada, kami hanya menerima cewek dan cowok.`), m)
+          client.reply(m.chat, `🚩 Sukses set gender kamu ke *${args[0]}*.`, m).then(() => user.gender = parseInt(args[0]))
+       } catch (e) {
+          client.reply(m.chat, Func.jsonFormat(e), m)
+       }
+    },
+    owner: true,
+    cache: true,
+    location: __filename
+ }
