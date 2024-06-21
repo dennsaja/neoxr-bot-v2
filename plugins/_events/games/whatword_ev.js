09a2b3a1cac2ba04e48bfdd90febe9f1523ed9e2
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-06-21 18:15:00 +0700

    fix

--- plugins/_events/games/whatword_ev.js
+++ plugins/_events/games/whatword_ev.js
@@ -9,6 +9,7 @@ exports.run = {
       try {
          var id = m.chat
          var reward = Func.randomInt(global.min_reward, global.max_reward)
+         let exif = global.db.setting
          client.whatword = client.whatword ? client.whatword : {}
          if (m.quoted && m.quoted.sender != client.decodeJid(client.user.id)) return
          if (m.quoted && /wordclue/i.test(m.quoted.text)) {
@@ -18,8 +19,8 @@ exports.run = {
                if (['Timeout', ''].includes(body)) return !0
                if (body.toLowerCase() == json.jawaban.toLowerCase()) {
                   await client.sendSticker(m.chat, await Func.fetchBuffer('./media/sticker/true.webp'), m, {
-                     packname: global.db.setting.sk_pack,
-                     author: global.db.setting.sk_author
+                     packname: exif.sk_pack,
+                     author: exif.sk_author
                   }).then(() => {
                      client.reply(m.chat, `*+ ${Func.formatNumber(reward)} Point*`, m)
                      users.point += reward
@@ -28,13 +29,13 @@ exports.run = {
                   })
                } else {
                   if (users.point == 0) return client.sendSticker(m.chat, await Func.fetchBuffer('./media/sticker/false.webp'), m, {
-                     packname: global.db.setting.sk_pack,
-                     author: global.db.setting.sk_author
+                     packname: exif.sk_pack,
+                     author: exif.sk_author
                   })
                   users.point < reward ? users.point = 0 : users.point -= reward
                   await client.sendSticker(m.chat, await Func.fetchBuffer('./media/sticker/false.webp'), m, {
-                     packname: global.db.setting.sk_pack,
-                     author: global.db.setting.sk_author
+                     packname: exif.sk_pack,
+                     author: exif.sk_author
                   }).then(() => {
                      client.reply(m.chat, `*- ${Func.formatNumber(reward)} Point*`, m)
                   })
